dba10504505161567cf16b43fcbaeb774964a505
Marcel Canhisares <marcel.canhisares@gmail.com>
2023-09-06 23:34:15 -0400

    Initial Commit

---
+++ Chessbic/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Chessbic
+//
+//  Created by Marcel Canhisares on 2023-09-06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
